43c6d953a47e9c42a941ef5058ca456b67387bb7
Mohmmad Fawzy <moha.fawzy63@gmail.com>
2025-08-19 05:37:54 +0000

    refactir: optimize code

--- src/BaseLines/BaseLine1_ImageClassification/dataset_splitter.py
+++ src/BaseLines/BaseLine1_ImageClassification/dataset_splitter.py
@@ -5,46 +5,17 @@ from src.enums.PathEnums import Paths
 from src.BaseLines.BaseLine1_ImageClassification.annotation import Annotation
 
 class DatasetSplitter:
-    """
-    Load data and split the dataset into train, validation, and test sets.
-    """
+    def __init__(self, train_ratio=0.6, valid_ratio=0.1):
+        # self.annotation_root = (Paths.ANNOT_PKL.value)
+        self.train_ratio = train_ratio
+        self.valid_ratio = valid_ratio
 
-    def __init__(self) -> None:
-        """
-        Initialize the DatasetSplitter.
-        Attributes:
-            output_root (str): The root directory for output data.
-            pkl_path (str): The path to the pickle file containing annotations.
+    def get_all_annotations(self):
 
-        """
-        self.output_root = Paths.OUTPUT_ROOT.value
-        self.pkl_path = Paths.ANNOT_PKL.value # annot_path
-        # self.pkl_path = os.path.join('../../', Paths.ANNOT_PKL.value)
-
-    def load_pkl(self) -> dict:
-        """
-        Load the pickle file containing video annotations.
-        returns:
-            dict: A dictionary containing video annotations.
-        """
-        with open(self.pkl_path, "rb") as f:
-            videos_annot = pickle.load(f)
-        return videos_annot
-
-
-    def build_annotations(self):
-        """
-        Build annotations from the loaded pickle file.
-        returns:
-            data_annot: A list of dictionaries containing frame paths and their categories.
-            labels: A list of unique category labels.
-            categories_dct: A dictionary mapping category labels to their indices.
-        """
-
-        videos_annot_dct = self.load_pkl()
-        # print(len(videos_annot_dct)) # 55
-        data_annot, labels = [], set()
-
+        with open(Paths.ANNOT_PKL.value, "rb") as f:
+            videos_annot_dct = pickle.load(f)
+        labels = set()
+        all_annotations = []
         for video_id, clips in videos_annot_dct.items(): # each video
 
             for clip_id, clip_data in clips.items():     # each clip
@@ -54,117 +25,63 @@ class DatasetSplitter:
 
                 for frame_id, boxes in clip_data["frame_boxes_dct"].items():    # Frames
                     frame_path = f"{Paths.VIDEOS_ROOT.value}/{video_id}/{clip_id}/{frame_id}.jpg"
-                    data_annot.append(
+                    all_annotations.append(
                         {
                             "path": frame_path, 
                             "category": category
                         }
                     )
-        labels = sorted(labels)
-        categories_dct = {label: idx for idx, label in enumerate(labels)}
+        return all_annotations, labels
 
-        # categories_dct = {
-        #     'l-pass': 0,
-        #     'r-pass': 1,
-        #     'l-spike': 2,
-        #     'r_spike': 3,
-        #     'l_set': 4,
-        #     'r_set': 5,
-        #     'l_winpoint': 6,
-        #     'r_winpoint': 7
-        # }
-        # labels = list(categories_dct.keys())
+    def split_dataset(self):
+        all_data, labels = self.get_all_annotations()
+        all_data_cpy = all_data.copy()
+        random.shuffle(all_data)
 
-        return data_annot, labels, categories_dct
+        n_total = len(all_data)
+        n_train = int(n_total * self.train_ratio)
+        n_valid = int(n_total * self.valid_ratio)
 
+        train_split = all_data[:n_train]
+        valid_split = all_data[n_train:n_train+n_valid]
+        test_split = all_data[n_train+n_valid:]
 
-    def split(self, data_annot, train_ratio=0.6, valid_ratio=0.3) -> tuple:
-        """
-        Split the dataset into train, validation, and test sets.
-        Args:
-            data_annot (list): The annotated data to split.
-            train_ratio (float): The proportion of data to use for training.
-            valid_ratio (float): The proportion of data to use for validation.
-        Returns:
-            tuple: A tuple containing the train, validation, and test splits.
-        """
-        random.shuffle(data_annot)
-        n_total = len(data_annot)
-        n_train = int(train_ratio * n_total)
-        n_valid = int(valid_ratio * n_total)
-        # print("split", n_total, n_train, n_valid, n_total-n_train-n_valid)
-        # 4370 || 26082 || 13041 || 4347
-        return (
-            data_annot[:n_train],
-            data_annot[n_train:n_train+n_valid],
-            data_annot[n_train+n_valid:]
-        )
+        return all_data_cpy, train_split, valid_split, test_split, labels
 
-    def save_split(self, data_split, split_path) -> None:
-        """
-        Save the split dataset to the specified path.
-        """
-        split_path.mkdir(parents=True, exist_ok=True)
-        count = 0
-        count_override = 0
-        for item in data_split:
+# splitter = DatasetSplitter()
+# train_split, valid_split, test_split, labels = splitter.split_dataset()
 
-            src = Path(item["path"])
-            label = item["category"]
-            dst_dir = split_path / label
-            dst_dir.mkdir(parents=True, exist_ok=True)
-            vid_num = Path(src).parents[1].name
-            dst = dst_dir / f"vid_{vid_num}_{os.path.basename(src)}"
-            
-            try:
-                if dst.exists():
-                    num = random.randint(1, 10)
-                    dst = dst_dir / f"vid_{vid_num}_{num}_{src.name}"
-                    count_override+=1
-
-                shutil.copy(src, dst)
-                count += 1
-
-            # try:
-            #     if not dst.exists():
-            #         shutil.copy(src, dst)
-            #         count += 1
-            #     else:
-            #         num = random.randint(1, 10)
-            #         dst = dst_dir / f"vid_{num}_{vid_num}_{os.path.basename(src)}"
-            #         shutil.copy(src, dst)
-            #         count += 1
-
-            except Exception as e:
-                print(f"Error copying {src} → {dst}: {e}")
-        print("save split", count, count_override)
-
+# len(data_annot), len(train_split), len(valid_split), len(test_split), len(labels), labels  # (43470, 26082, 4347, 13041, 8)
 
 if __name__ == "__main__":
 
+    splitter = DatasetSplitter()
+    all_data, train_split, valid_split, test_split, labels = splitter.split_dataset()
 
-    annot_loader = DatasetSplitter()
-    data_annot, labels, categories_dct = annot_loader.build_annotations()
+    print(f"len data: {len(all_data)} || train: {len(train_split)} || valid: {len(valid_split)} || test: {len(test_split)}")
 
-    # print(f"{len(labels)} labels: {labels}\n\n, {categories_dct}")
-    for frame_dct in data_annot:
+    print(train_split[0], "\n")
+
+    for frame_dct in all_data:
         for path, cat in frame_dct.items():
             print(frame_dct["path"], "==>", frame_dct["category"])
             break
         break
     
-    train_data, valid_data, test_data = annot_loader.split(data_annot)
+    
+
+
     # print(int(len(data_annot)*0.6) +  int(len(data_annot)*0.3) + int(len(data_annot)*0.1)) # 43470
 
-    print(Paths.TRAIN_PATH.value, Paths.VALID_PATH.value, Paths.TEST_PATH.value)
+    # print(Paths.TRAIN_PATH.value, Paths.VALID_PATH.value, Paths.TEST_PATH.value)
 
-    print("Train...") # 25973
-    annot_loader.save_split(train_data, Paths.TRAIN_PATH.value)
-    print("Valid...") # 12861
-    annot_loader.save_split(valid_data, Paths.VALID_PATH.value)
-    print("Test...") # 4300
-    annot_loader.save_split(test_data, Paths.TEST_PATH.value)
-    print(Paths.TRAIN_PATH.value, Paths.VALID_PATH.value, Paths.TEST_PATH.value)
+    # print("Train...") # 25973
+    # annot_loader.save_split(train_data, Paths.TRAIN_PATH.value)
+    # print("Valid...") # 12861
+    # annot_loader.save_split(valid_data, Paths.VALID_PATH.value)
+    # print("Test...") # 4300
+    # annot_loader.save_split(test_data, Paths.TEST_PATH.value)
+    # print(Paths.TRAIN_PATH.value, Paths.VALID_PATH.value, Paths.TEST_PATH.value)
 
 
 # python -m src.BaseLines.BaseLine1_ImageClassification.dataset_splitter
--- src/BaseLines/BaseLine1_ImageClassification/image_classification.py
+++ src/BaseLines/BaseLine1_ImageClassification/image_classification.py
@@ -1,6 +1,5 @@
 """
 """
-
 from src.BaseLines.BaseLine1_ImageClassification.dataset_splitter import DatasetSplitter
 from src.enums.PathEnums import Paths
 import torch
@@ -8,94 +7,123 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from pathlib import Path
 from PIL import Image
+import os
+
+# class ImageFolderCustom(Dataset):
+#     def __init__(self, dir, labels, categories_dct, transform=None):
+#         """
+#         Initialize the dataset.
+#         Args:
+#             dir (str): The directory containing the images.
+#             labels (list): The list of class labels.
+#             categories_dct (dict): The dictionary mapping class labels to indices.
+#             transform : A function/transform to apply to the images.
+#         """
+
+#         self.paths = self.get_list_of_files(dir)
+#         # print(len(self.paths))
+#         self.transform = transform
+#         self.classes, self.class_to_idx = labels, categories_dct  # self.get_classes()
+#         # self.labels = labels
+#         # self.category_dict = categories_dct
 
 
-class ImageFolderCustom(Dataset):
-    def __init__(self, dir, labels, categories_dct, transform=None):
-        """
-        Initialize the dataset.
-        Args:
-            dir (str): The directory containing the images.
-            labels (list): The list of class labels.
-            categories_dct (dict): The dictionary mapping class labels to indices.
-            transform : A function/transform to apply to the images.
-        """
-
-        self.paths = self.get_list_of_files(dir)
-        # print(len(self.paths))
-        self.transform = transform
-        self.classes, self.class_to_idx = labels, categories_dct  # self.get_classes()
-        # self.labels = labels
-        # self.category_dict = categories_dct
-
-
-    def get_list_of_files(self, dir_path) -> list[str]:
-        """
-        Get a list of all image files in the directory.
-        Args:
-            dir_path (str): The directory path to search for image files.
-        Returns:
-            list[str]: A list of image file paths.
-        """
-        return list(Path(dir_path).glob("*/*.jpg"))
+#     def get_list_of_files(self, dir_path) -> list[str]:
+#         """
+#         Get a list of all image files in the directory.
+#         Args:
+#             dir_path (str): The directory path to search for image files.
+#         Returns:
+#             list[str]: A list of image file paths.
+#         """
+#         return list(Path(dir_path).glob("*/*.jpg"))
     
-    # def get_classes(self):
+#     # def get_classes(self):
 
-    #     categories_dct = {
-    #     'l-pass': 0,
-    #     'r-pass': 1,
-    #     'l-spike': 2,
-    #     'r_spike': 3,
-    #     'l_set': 4,
-    #     'r_set': 5,
-    #     'l_winpoint': 6,
-    #     'r_winpoint': 7
-    #     }
+#     #     categories_dct = {
+#     #     'l-pass': 0,
+#     #     'r-pass': 1,
+#     #     'l-spike': 2,
+#     #     'r_spike': 3,
+#     #     'l_set': 4,
+#     #     'r_set': 5,
+#     #     'l_winpoint': 6,
+#     #     'r_winpoint': 7
+#     #     }
 
-    #     labels = list(categories_dct.keys())
+#     #     labels = list(categories_dct.keys())
 
-    #     return labels, categories_dct
+#     #     return labels, categories_dct
 
-    def load_image(self, index: int) -> Image:
-        """Load an image from a file path.
-        Args:
-            index (int): The index of the image to load.
-        Returns:
-            Image: The loaded image.
+#     def load_image(self, index: int) -> Image:
+#         """Load an image from a file path.
+#         Args:
+#             index (int): The index of the image to load.
+#         Returns:
+#             Image: The loaded image.
+#         """
+#         image_path = self.paths[index]
+#         return Image.open(image_path)
+
+#     def __len__(self) -> int:
+#         "Returns the total number of samples."
+#         return len(self.paths)
+
+#     def __getitem__(self, index: int) -> tuple[Image, int]:
+#         "Returns one sample of data, data and label (X, y)."
+#         img = self.load_image(index)
+#         class_name  = self.paths[index].parent.name # expects path ::>> data_folder/class_name/image.jpeg
+#         class_idx = self.class_to_idx[class_name]
+
+#         # Transform if necessary
+#         if self.transform:
+#             return self.transform(img), class_idx # return data, label (X, y)
+#         else:
+#             return (img, class_idx) # return data, label (X, y)
+
+# =======================================================================================================
+
+
+class CustomDataset(Dataset):
+    def __init__(self, data_split, labels, transform=None):
         """
-        image_path = self.paths[index]
-        return Image.open(image_path)
+        Args:
+            data_split: list of dicts [{"path": ..., "category": ...}, ...]
+            labels: full list of class labels
+            transform: torchvision transforms
+        """
+        self.data_split = data_split
+        self.labels = labels
+        self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(labels)}
+        self.transform = transform
 
-    def __len__(self) -> int:
-        "Returns the total number of samples."
-        return len(self.paths)
+    def __len__(self):
+        return len(self.data_split)
 
-    def __getitem__(self, index: int) -> tuple[Image, int]:
-        "Returns one sample of data, data and label (X, y)."
-        img = self.load_image(index)
-        class_name  = self.paths[index].parent.name # expects path ::>> data_folder/class_name/image.jpeg
-        class_idx = self.class_to_idx[class_name]
-
-        # Transform if necessary
+    def __getitem__(self, idx):
+        item = self.data_split[idx]
+        # img = Image.open(item["path"]).convert("RGB")
+        img = Image.open(item["path"]).convert("RGB")
+        label = self.class_to_idx[item["category"]]
         if self.transform:
-            return self.transform(img), class_idx # return data, label (X, y)
-        else:
-            return (img, class_idx) # return data, label (X, y)
+            img = self.transform(img)
+        return img, label
 
 
 
 if __name__ == "__main__":
-    annot_loader = DatasetSplitter()
-    data_annot, labels, categories_dct = annot_loader.build_annotations()
+    splitter = DatasetSplitter()
+    all_data, train_split, valid_split, test_split, labels = splitter.split_dataset()
 
-    # train_data, valid_data, test_data = annot_loader.split(data_annot)
-    # # print(int(len(data_annot)*0.6) +  int(len(data_annot)*0.3) + int(len(data_annot)*0.1)) # 43470
-    # print("Train...") # 25973
-    # annot_loader.save_split(train_data, Paths.TRAIN_PATH.value)
-    # print("Valid...") # 12861
-    # annot_loader.save_split(valid_data, Paths.VALID_PATH.value)
-    # print("Test...") # 4300
-    # annot_loader.save_split(test_data, Paths.TEST_PATH.value)
+    print(f"len data: {len(all_data)} || train: {len(train_split)} || valid: {len(valid_split)} || test: {len(test_split)}")
+
+    print(train_split[0], "\n")
+
+    for frame_dct in all_data:
+        for path, cat in frame_dct.items():
+            print(frame_dct["path"], "==>", frame_dct["category"])
+            break
+        break
     # ====================================================================
 
     print("transform")
@@ -116,65 +144,47 @@ if __name__ == "__main__":
         ])
 
 
-    train_data_custom = ImageFolderCustom(dir=Paths.TRAIN_PATH.value,
-                                          labels=labels, categories_dct=categories_dct,
-                                          transform=train_transforms
-                                          )
+    train_dataset = CustomDataset(train_split, labels, transform=train_transforms)
+    valid_dataset = CustomDataset(valid_split, labels, transform=test_transforms)
+    test_dataset  = CustomDataset(test_split,  labels, transform=test_transforms)
 
-    valid_data_custom = ImageFolderCustom(dir=Paths.VALID_PATH.value,
-                                          labels=labels, 
-                                          categories_dct=categories_dct,
-                                          transform=test_transforms
-                                          )
 
-    test_data_custom = ImageFolderCustom(dir=Paths.TEST_PATH.value, 
-                                         labels=labels, 
-                                         categories_dct=categories_dct,
-                                         transform=test_transforms
-                                         )
+    print(f"len train : {len(train_dataset)}")
+    print(f"len valid : {len(valid_dataset)}")
+    print(f"len test : {len(test_dataset)}")  
+    #   # (26082, 13041, 4347)
 
-    print(f"len train : {len(train_data_custom)}")
-    print(f"len valid : {len(valid_data_custom)}")
-    print(f"len test : {len(test_data_custom)}")  
-      # (26082, 13041, 4347)
+    print(valid_dataset.labels)
+    print(valid_dataset.class_to_idx)
+    print("------------------------------------")
 
-    image, label = train_data_custom[0]
-    class_names = train_data_custom.classes
-    class_to_idx = train_data_custom.class_to_idx
-    print(class_names, class_to_idx)
+    image, label = train_dataset[0]
+
+    class_names = list(train_dataset.labels)
+    class_to_idx = train_dataset.class_to_idx
     print(f"classes : {class_names} \n classes idx : {class_to_idx}\n")
-    print(f"image shape : {image.shape}")
+    print(f"image shape : {image.shape}, label:{label} => {class_names[label]}")
 
 
-    BATCH_SIZE = 32
-    train_dataloader = DataLoader(dataset=train_data_custom, 
-                                     batch_size=BATCH_SIZE, # how many samples per batch?
-                                     num_workers=0, # how many subprocesses to use for data loading? (higher = more)
-                                     shuffle=True) # shuffle the data?
-
-    test_dataloader = DataLoader(dataset=test_data_custom, # use custom created test Dataset
-                                        batch_size=BATCH_SIZE, 
-                                        num_workers=0, 
-                                        shuffle=False) # don't usually need to shuffle testing data
-
-    valid_dataloader = DataLoader(dataset=valid_data_custom, # use custom created valid Dataset
-                                         batch_size=BATCH_SIZE, 
-                                         num_workers=0,
-                                         shuffle=False) # don't usually need to shuffle validation data
-
-    print(f"Length of train dataloader: {len(train_dataloader)} batches of {BATCH_SIZE}") #=> 816 || 32
-    print(f"Length of test dataloader: {len(test_dataloader)} batches of {BATCH_SIZE}")   #=> 136 || 32
-    print(f"Length of valid dataloader: {len(valid_dataloader)} batches of {BATCH_SIZE}") #=> 408 || 32
+    # BATCH_SIZE = 32
+    # train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
+    # valid_loader = DataLoader(valid_dataset, batch_size=32, shuffle=False, num_workers=4)
+    # test_loader  = DataLoader(test_dataset,  batch_size=32, shuffle=False, num_workers=4)
 
 
-    print("=========================")
-    for batch, i in enumerate (train_dataloader):  # c (Counter), i ==> [Batch([32, 1, 28, 28]), labels([32])]
-        print("Batch", batch, "===> ", i[0].shape, i[1].shape)
-        if batch == 5:
-            break
+    # print(f"Length of train dataloader: {len(train_dataloader)} batches of {BATCH_SIZE}") #=> 816 || 32
+    # print(f"Length of test dataloader: {len(test_dataloader)} batches of {BATCH_SIZE}")   #=> 136 || 32
+    # print(f"Length of valid dataloader: {len(valid_dataloader)} batches of {BATCH_SIZE}") #=> 408 || 32
 
-    train_features_batch, train_labels_batch = next(iter(train_dataloader))
-    print(train_features_batch.shape, train_labels_batch.shape)
+
+    # print("=========================")
+    # for batch, i in enumerate (train_dataloader):  # c (Counter), i ==> [Batch([32, 1, 28, 28]), labels([32])]
+    #     print("Batch", batch, "===> ", i[0].shape, i[1].shape)
+    #     if batch == 5:
+    #         break
+
+    # train_features_batch, train_labels_batch = next(iter(train_dataloader))
+    # print(train_features_batch.shape, train_labels_batch.shape)
 
 
 # python -m src.BaseLines.BaseLine1_ImageClassification.image_classification
